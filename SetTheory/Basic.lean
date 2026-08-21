a5eef410651bc09b6f9606108f398a0a6770ea54
Seraphina Singer <esingeruk@gmail.com>
2026-08-21 17:08:54 +0000

    Commit

--- SetTheory/Basic.lean
+++ SetTheory/Basic.lean
@@ -1,1 +1,30 @@
 import SetTheory.Axioms
+
+theorem eq_iff_same_elements (X Y : V) : X = Y ↔ ∀ u : V, u ∈ X ↔ u ∈ Y := by
+  constructor
+  . intro h
+    intro u
+    rw [h]
+  . intro h
+    exact ax_extensionality h
+
+theorem subset_self (X : V) : X ⊆ X := by
+  unfold is_subset
+  intro u
+  exact u.property
+
+theorem eq_iff_subsets (X Y : V) : X = Y ↔ X ⊆ Y ∧ Y ⊆ X := by
+  constructor
+  . intro h
+    rw [h]
+    exact ⟨subset_self Y, subset_self Y⟩
+  . intro h
+    unfold is_subset at h
+    obtain ⟨h1, h2⟩ := h
+    rw [eq_iff_same_elements]
+    intro u
+    constructor
+    . intro h
+      exact h1 ⟨u, h⟩
+    . intro h
+      exact h2 ⟨u, h⟩
--- SetTheory/Ordinals.lean
+++ SetTheory/Ordinals.lean
@@ -10,5 +10,14 @@ def l_order : V → Prop := fun X => (p_order X ∧ ∀ x y : X, (x ≠ y → (x
 def least : V → V → Prop := fun u => (fun X => (u ∈ X ∧ ∀ x : X, u ≠ x → u ∈ x))
 def w_order : V → Prop := fun X => (l_order X ∧ ∀ U : 𝒫 X, U ≠ ∅ → ∃ u : V, least u U)
 def transitive : V → Prop := fun X => (∀ x : X, x ⊆ X)
-def is_ordinal : V → Prop := fun X => (w_order X ∧ transitive X)
-def On : Type := {X : V // is_ordinal X}
+def is_ord : V → Prop := fun X => (w_order X ∧ transitive X)
+def On : Type := {X : V // is_ord X}
+
+instance : Coe On V where
+  coe α := α.val
+
+theorem succ_p_order_is_p_order {X : V} : p_order X → p_order (succ X) := by
+  unfold p_order
+  intro h
+  intro x y z 
+  sorry
